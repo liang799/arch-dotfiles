c2c630d486fe72e0a9ef5d1aaed14aa9be492df0
liang799 <tp@archbox.localdomain>
2021-09-27 03:09:34 +0800

    updated gitignore and removed files

--- bspwm/nippleInput.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ $(xinput list-props 11 | grep 'Device Enabled' | gawk -F ': ' '{ print $2 }') -eq 0 ]; then
-	xinput enable 11
-	dunstify --icon=/usr/share/icons/mate/scalable/actions/touchpad-enabled.svg "Enabled" "Your computer's touchpad is enabled."
-else
-	xinput disable 11
-	dunstify --icon=/usr/share/icons/mate/scalable/actions/touchpad-disabled.svg "Disabled" "Your computer's touchpad is disabled."
-fi
